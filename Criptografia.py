53917a5d4295980ca58e62d4b966e17e52633951
Filipe-Santos-Soares <filipinhosoaressantos@gmail.com>
2025-09-04 14:52:47 -0300

    ajustando programa criptografador

--- Criptografia.py
+++ Criptografia.py
@@ -2,16 +2,30 @@ class Criptografador:
     def __init__(self, texto):
         self.texto = texto
 
-    def substituir_vogais(self):
+    def criptografar(self):
         #A – 4, E – 3, I – 1, O – 0, U – 8
-        novo_texto = str.maketrans("aeiouAEIOU", "4310843108")
-        return self.texto.translate(novo_texto)
+        substituicoes = {
+            "A": "4", "a": "4",
+            "e": "3", "E": "3",
+            "i": "1", "I": "1",
+            "o": "0", "O": "0",
+            "u": "8", "U": "8"
+        }
+        frase_criptografada = ""
+        for caracter in self.texto:
+            if caracter in substituicoes:
+                frase_criptografada += substituicoes[caracter]
+            else:
+                frase_criptografada += caracter
+            
+        return frase_criptografada
     
 
 def main():
-    texto = input("\nDigite um texto: ")
+    texto = input("\nDigite um texto para criptografar: ")
     criptografia = Criptografador(texto)
-    print(f"\nTexto criptografado: {criptografia.substituir_vogais()}")  
+    resultado = criptografia.criptografar()
+    print(f"\nTexto criptografado: '{resultado}'")  
 
 
 if __name__ =="__main__":
